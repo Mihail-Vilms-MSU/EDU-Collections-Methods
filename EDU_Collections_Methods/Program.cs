6ced03110aea6f524c78265d5da074e3bfee63d9
Mikhail <mihail.vilms@gmail.com>
2019-10-24 18:11:59 -0500

    HW project

---
+++ EDU_Collections_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EDU_Collections_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
